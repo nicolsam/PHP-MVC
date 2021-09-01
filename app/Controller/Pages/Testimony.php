fcedad5eafc159aa1b2009a6a05f07700578dda4
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-09-01 17:40:42 -0300

    Classe controladora de Depoimentos agora pode fazer inserções no BD

--- app/Controller/Pages/Testimony.php
+++ app/Controller/Pages/Testimony.php
@@ -3,10 +3,19 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
-use \App\Model\Entity\Organization;
+use \App\Model\Entity\Testimony as EntityTestimony;
 
 class Testimony extends Page {
 
+    /**
+     * Método responsável por obter a renderização dos itens de depoimentos para a página
+     *
+     * @return [type]
+     *   [return description]
+     */
+    private static function getTestimonyItems() {
+
+    }
     /**
      * Método responsável por retornar o conteúdo (view) de depoimentos
      *
@@ -14,9 +23,32 @@ class Testimony extends Page {
      */
     public static function getTestimonies() {
         // View de DEPOIMENTOS
-        $content = View::render('pages/testimonies', []);
+        $content = View::render('pages/testimonies', [
+            'itens' => self::getTestimonyItems()
+        ]);
 
         // Retorna a View de DEPOIMENTOS
         return parent::getPage('DEPOIMENTOS', $content);
+    }
+
+    /**
+     * Método responsável por cadastrar um depoimento
+     *
+     * @param Request $request
+     *
+     * @return string
+     */
+    public static function insertTestimony($request) {
+        // Dados do POST
+        $postVars = $request->getPostVars();
+
+        // Nova instância de depoimento
+        $obTestimony = new EntityTestimony;
+        $obTestimony->nome = $postVars['nome'];
+        $obTestimony->mensagem = $postVars['mensagem'];
+        
+        $obTestimony->cadastrar();
+        
+        return self::getTestimonies();
     }
 }
