f5065b5b683e2bb7a7644a7db6f772d1daee74dc
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-08-31 22:22:03 -0300

    Adicionado variáveis de configuração para serem utilizados na Utils/View

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use \App\Common\Environment;
+use \App\Utils\View;
+use \App\Bd\Model\Database;
 
 // Carrega as variáveis de ambiente do projeto
 Environment::load(__DIR__ . "/../");
+
+define('URL', getenv('URL'));
+
+// Define o valor padrão das variáveis
+View::init([
+    'URL' => URL
+]);
