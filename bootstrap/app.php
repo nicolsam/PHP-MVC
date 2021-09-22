91afb5d751c665a9d816daa00715b4e743237dc0
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-09-22 20:13:49 -0300

    Adicionado lista de middlewares e lista de middlewares padrões (para todas as páginas)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use \App\Common\Environment;
 use \App\Utils\View;
 use \App\Bd\Model\Database;
+use \App\Http\Middleware\Queue as MiddlewareQueue;
 
 // Carrega as variáveis de ambiente do projeto
 Environment::load(__DIR__ . "/../");
@@ -15,4 +16,14 @@ define('URL', getenv('URL'));
 // Define o valor padrão das variáveis
 View::init([
     'URL' => URL
+]);
+
+// Define o mapeamento de middlewares
+MiddlewareQueue::setMap([
+    'maintenance' => \App\Http\Middleware\Maintenance::class
+]);
+
+// Define o mapeamento de middlewares padrões, executado em todas as rotas
+MiddlewareQueue::setDefault([
+    'maintenance'
 ]);
