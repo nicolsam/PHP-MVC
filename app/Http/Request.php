c7d19aa48e97edc1d17a56d380b6362a6e74e9b0
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-09-22 11:49:02 -0300

    Uri configurada corretamente

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -5,6 +5,12 @@ namespace App\Http;
 class Request {
 
     /**
+     * Instância do Router
+     * @var Router
+     */
+    private $router;
+
+    /**
      * Método Http da requisição
      *
      * @var string 
@@ -42,14 +48,41 @@ class Request {
     /** 
      * Construtor da classe
      */
-    public function __construct() {
+    public function __construct($router) {
+        $this->router      = $router;
         $this->queryParams = $_GET ?? [];
         $this->postVars    = $_POST ?? [];
 
-        $this->headers     = getallheaders();
+        $this->headers = getallheaders();
 
-        $this->httpMethod  = $_SERVER['REQUEST_METHOD'] ?? '';
-        $this->uri         = $_SERVER['REQUEST_URI'] ?? '';
+        $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
+        $this->setUri();
+    }
+
+    /**
+     * Método responsável por definir a URI
+     *
+     * @return [type]
+     *   [return description]
+     */
+    private function setUri() {
+        // URI completa (com GETs)
+        $this->uri = $_SERVER['REQUEST_URI'] ?? '';
+
+        // Remove GETs da URI
+        $xURI = explode('?', $this->uri);
+
+        $this->uri = $xURI[0];
+
+    }
+
+    /**
+     * Método responsável por retornar a instância de Router
+     *
+     * @return Router
+     */
+    public function getRouter() {
+        return $this->router;
     }
 
     /**
